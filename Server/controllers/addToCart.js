473a88590c82a75ebc78bc778b27e1102f40c01b
GovindaEkbote <govindaekbote7@gmail.com>
2025-02-10 07:49:40 +0530

    Update add to cart api

--- Server/controllers/addToCart.js
+++ Server/controllers/addToCart.js
@@ -3,59 +3,71 @@ const ErrorHandler = require("../utils/ErrorHandling");
 const tryCatch = require("../middleware/tryCatch");
 const Cart = require("../models/cartModel");
 
+// Add Product into cart..
 exports.addToCart = tryCatch(async (req, res, next) => {
-    const { productId, quantity } = req.body;
-  
-    if (!req.user || !req.user._id) {
-      return next(new ErrorHandler("User is not authenticated", 401));
-    }
-    const userId = req.user._id;
-     
-    const product = await Medicine.findById(productId);
-    if (!product) {
-      return next(new ErrorHandler("Product Not Found", 404));
-    }
-    if (product.stock < quantity) {
-      return next(new ErrorHandler(`Only ${product.stock} units available`, 400));
-    }
-  
-    let cart = await Cart.findOne({ user: userId });
-    if (!cart) {
-      cart = new Cart({ user: userId, cartItems: [] });
-    }
-  
-    const existingItem = cart.cartItems.find((item) => item.product.equals(productId));
-    if (existingItem) {
-      existingItem.quantity += quantity;
-    } else {
-      cart.cartItems.push({
-        product: productId,
-        name: product.name,
-        price: product.price,
-        quantity,
-        images: product.images,
-      });
-    }
-  
-    // Reduce product stock
-    product.stock -= quantity;
-    await product.save();
-  
-    await cart.save();
-  
-    const updatedCart = await Cart.findById(cart._id).populate(
-      "cartItems.product",
-      "name price stock"
-    );
-  
-    res.status(200).json({
-      success: true,
-      message: "Item added to cart successfully",
-      cart: updatedCart,
+  const { productId } = req.body;
+
+  if (!req.user || !req.user._id) {
+    return next(new ErrorHandler("User is not authenticated", 401));
+  }
+  const userId = req.user._id;
+
+  const product = await Medicine.findById(productId);
+  if (!product) {
+    return next(new ErrorHandler("Product Not Found", 404));
+  }
+  if (product.stock < 1) {
+    return next(new ErrorHandler(`Only ${product.stock} units available`, 400));
+  }
+
+  let cart = await Cart.findOne({ user: userId });
+  if (!cart) {
+    cart = new Cart({ user: userId, cartItems: [] });
+  }
+
+  const existingItem = cart.cartItems.find((item) =>
+    item.product.equals(productId)
+  );
+  if (existingItem) {
+    existingItem.quantity += 1;
+  } else {
+    cart.cartItems.push({
+      product: productId,
+      name: product.name,
+      price: product.price,
+      quantity: 1,
+      images: product.images,
     });
+  }
+
+  await cart.save();
+
+  const updatedCart = await Cart.findById(cart._id).populate(
+    "cartItems.product",
+    "name price stock"
+  );
+
+  res.status(200).json({
+    success: true,
+    message: "Item added to cart successfully",
+    cart: updatedCart,
+  });
 });
 
+exports.updateProductQuantity = tryCatch(async(req,res,next)=>{
+   const {productId,action} = req.body;
 
-  
+   if(!req.user || !req.user._id){
+    return next(new ErrorHandler("User is not authenticated",401));
+   }
+   const userId = req.user._id;
 
-  
+   let cart =await Cart.findOne({user:userId})
+   if(!cart)
+   {
+    return next(new ErrorHandler("cart not dound",404));
+   }
+
+})
+
+
